4a77823b2508e20f4f95f652d339ead42c742a32
TheMimikyu <angelwithadream@gmail.com>
2023-03-06 17:32:52 +0530

    modified:   Programming/001 Basics.cpp 	modified:   Programming/001 Basics.exe

--- Programming/001 Basics.cpp
+++ Programming/001 Basics.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 // iostream is the header(file) where cout implementation defined.
+using namespace std;
 int main()
 {
    // std is standard namespace.
    // namespace is the region where the functions scope is defined.
-    std::cout << "Hello World" ;
+    //std::cout << "Hello World" ;
+    // \n endl to break lines.
+    cout << "Hello World \n";
+    // cin for getting input
+    int a;
+    // cout << sizeof(a) << endl;
+    // << for output
+    // >> for inputs
+    cin >> a;
+    cout << a << endl;
+    // +ve No.s are stored and data begins from 0.
+    // While for negative data begins from 1.
+    // To the negative of a positive no. 0101101 will be 1010011 where we revesre every digit except the last.
+    // no. of Bytes read will be due to the Datatypes. int reads 4 bytes float reads 8.
+    // signed data type includes +ve , -ve and 0.
+    // unsigned only +ve.
+    // For Calculation of Range. Say we have 4bytes (int). No of bits would be 32. Then Total combination will be 2^32. 
+    // Then For 
 }
